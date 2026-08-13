627162310f532eaad751e3598a2ad57efad7bab4
Madhur <madhurchaturvedi2000@gmail.com>
2026-08-13 23:42:08 +0530

    js_3 1challange solve

---
+++ JavaScript/JS3/index.js
@@ -0,0 +1,15 @@
+
+
+let product = 150;
+let quantity = 3;
+
+let totalCost = product * 3;
+let discount = totalCost * 0.10;
+let discountPrice = totalCost - discount;
+
+
+console.log("price per Item:", product);
+console.log("price quantity:", quantity);
+console.log("price per totalCost:", totalCost);
+console.log("price per discount:", discount);
+console.log("price per discountPrice:", discountPrice);
