54e2ba72549b2f0cb8d31f8cf7ef42f538e8c6aa
Madhur <madhurchaturvedi2000@gmail.com>
2026-08-14 01:08:11 +0530

    Add initial implementations of age voting check and user authentication logic

---
+++ JavaScript/JS3/Challenge/index.js
@@ -0,0 +1,7 @@
+
+
+let age = 19;
+
+let result = age >= 18 ? "Can Vote 🟢" : "Can not Vote 🔴"
+
+console.log(result)
---
+++ JavaScript/JS3/Challenge/index2.js
@@ -0,0 +1,9 @@
+
+// Check the usename and passworld shoub be not empty
+
+let username = "madhur";
+let password = "1234";
+
+let auth = (username && password) ? "Login" : "not Authorized";
+
+console.log(auth)
