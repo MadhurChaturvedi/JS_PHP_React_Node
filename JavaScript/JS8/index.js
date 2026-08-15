7115e404efd8fa9541c89a49904d7281df7f268a
Madhur <madhurchaturvedi2000@gmail.com>
2026-08-16 01:30:37 +0530

    Refactor code structure for improved readability and maintainability

--- JavaScript/JS8/index.js
+++ JavaScript/JS8/index.js
@@ -5,3 +5,13 @@ let name = "madhur"
 let age = 25
 let nameAge = `my Name is ${name} and My age is : ${age} `
 console.log(nameAge)
+
+// String Constructor method to create strign
+
+let str = new String(1);
+// type is Object whey pata nhi
+console.log(typeof str)
+console.log(str)
+
+
+//String are imutable you can not change
