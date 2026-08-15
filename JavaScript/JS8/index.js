cb29f19feafb3a204b4186ba065f77ffaf552654
Madhur <madhurchaturvedi2000@gmail.com>
2026-08-15 23:59:56 +0530

    Add initial JavaScript templet litrals with string interpolation example

---
+++ JavaScript/JS8/index.js
@@ -0,0 +1,7 @@
+// we use Backticks in code `` <- this is called String litricals
+// the value/variable we use inside on the litrals called String interpolation
+
+let name = "madhur"
+let age = 25
+let nameAge = `my Name is ${name} and My age is : ${age} `
+console.log(nameAge)
