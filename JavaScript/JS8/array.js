21ac9f69d84807016e7c43f42ea3cc188c8c55bb
Madhur <madhurchaturvedi2000@gmail.com>
2026-08-17 01:04:30 +0530

    array

--- JavaScript/JS8/array.js
+++ JavaScript/JS8/array.js
@@ -5,4 +5,4 @@ let arr2 = ["max", 2, ["madhur", 26]];
 console.log(arr2[2][0]);
 let arr3 = new Array(1, 2, NaN, undefined, 3.12, true, "Fuck")
 console.log(arr3)
-// array is mutable  - indexing in array from 0 to N last ement is -1 -2 -3 -1 0 
+// array is mutable  - indexing in array from 0 to N last ement is -1 -2 -3 -1 0 is type of object
