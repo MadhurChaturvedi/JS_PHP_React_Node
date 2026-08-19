d73f7183cc71223bb12113c024695d4388ec6b86
Madhur <madhurchaturvedi2000@gmail.com>
2026-08-19 21:17:52 +0530

    solve the array full with zero

--- JavaScript/JS8/array/assignment.js
+++ JavaScript/JS8/array/assignment.js
@@ -1,17 +1,26 @@
 // sum of arr
 
-let arr = [1, 2, 3, 4, 5, 6];
+// let arr = [1, 2, 3, 4, 5, 6];
 
-// Accumulator and current value
-let sumarr = arr.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue
-})
+// // Accumulator and current value
+// let sumarr = arr.reduce((accumulator, currentValue) => {
+//     return accumulator + currentValue
+// })
 
-console.log(sumarr)
+// console.log(sumarr)
 
 
-let array = new Array(25);
-let ZeroArray = array.map((item, index) => {
-    return item = 0;
-})
+// let array = new Array(25);
+// let ZeroArray = array.map((item, index) => {
+//     return item = 0;
+// })
 
+
+// let newArr = new Array(27);
+// newArr.fill(0)
+// newArr.map((item, _) => {
+//     console.log(item)
+// })
+
+
+let use = Number(prompt("Enter the Number"))
