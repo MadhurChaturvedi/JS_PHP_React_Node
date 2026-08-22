14e7e14db8036d5990ee292a178960ceb35fd0d4
Madhur <madhurchaturvedi2000@gmail.com>
2026-08-22 10:57:17 +0530

    delete obj element

--- JavaScript/JS8/object.js
+++ JavaScript/JS8/object.js
@@ -1,9 +1,16 @@
 let obj = {
     "name": "madhur",
     start: () => console.log("Start The Function"),
-    "full name": `${this.name}`
+    "full name": `${this.obj}`
 }
 console.log(obj.name)
 // console.log(obj["name"])
 // obj["start"]()
 obj["full name"]
+
+// object is mutable which means wae can add and remove propertyes
+obj.email = "madhurchaturvedi2000@gmail.com"
+//deleate and elemnt of the object  
+delete obj.email
+
+console.log(obj)
