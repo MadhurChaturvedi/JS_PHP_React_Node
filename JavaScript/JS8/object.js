6ea2ef737c7a95165de227360c3e3c57d8005cfa
Madhur <madhurchaturvedi2000@gmail.com>
2026-08-24 00:33:02 +0530

    implement this in method of object ()=> // this one no have there own this only funcion have learn spread opratior add tow object single obj how to do it with constructor

--- JavaScript/JS8/object.js
+++ JavaScript/JS8/object.js
@@ -1,16 +1,29 @@
 let obj = {
-    "name": "madhur",
+    "firstname": "madhur",
     start: () => console.log("Start The Function"),
-    "full name": `${this.obj}`
+    "full name": function () {
+        console.log(this.firstname)
+    }
 }
-console.log(obj.name)
+// console.log(obj.firstname)
 // console.log(obj["name"])
 // obj["start"]()
-obj["full name"]
+// obj["full name"]
 
 // object is mutable which means wae can add and remove propertyes
 obj.email = "madhurchaturvedi2000@gmail.com"
 //deleate and elemnt of the object  
 delete obj.email
-console.log(obj)
+// console.log(obj)
+obj['full name']()
 
+let obj2 = {};
+
+let arobj = Object.assign(obj2, obj) // return modified array
+// console.log(arobj)
+
+let obj3 = { ...obj2, ...obj }
+console.log(obj3)
+
+
+// seal and freez
